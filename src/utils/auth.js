290b0f33ea144af696571d143186cd799315792f
Luiz Kim <luiz.kim@controleonline.com>
2025-02-21 11:43:03 -0300

    Update

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,3 +1,5 @@
+import { LocalStorage } from "quasar";
+
 export default class Auth {
   constructor(store, router) {
     this.store = store;
@@ -8,6 +10,11 @@ export default class Auth {
   logout(noReload = false) {
     this.store.dispatch("auth/logOut");
     if (!noReload) location.reload();
+  }
+  persist(user) {
+    LocalStorage.set("session", user);
+    this.store.commit("auth/LOGIN_SET_USER", user);
+    this.user = user;
   }
   toLogin() {
     this.$router.push({
