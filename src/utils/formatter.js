c745e3989e5419ea4f131fe5a78af1e2b01f3f14
Luiz Kim <luiz.kim@controleonline.com>
2025-03-16 20:01:01 -0300

    Update

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -105,6 +105,15 @@ export default class Formatter {
 
     return `${yyyy}-${mm}-${dd}`;
   }
+  
+  static getCurrentDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+  }
 
   static formatDateYmdTodmY(value, withTime = false) {
     if (!value) return '';
