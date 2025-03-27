6cc4ff1c524b52edabba663d6a8f0d201729ce04
Luiz Kim <luiz.kim@controleonline.com>
2025-03-27 17:13:30 -0300

    Update

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,30 +1,36 @@
-import {APP_ENV} from '../../../../../config/env.js';
+import { APP_ENV } from "../../../../../config/env.js";
 
 export default function (resourceEndpoint, options = {}) {
   const entryPoint =
-    APP_ENV.API_ENTRYPOINT + (APP_ENV.API_ENTRYPOINT.endsWith('/') ? '' : '/');
+    APP_ENV.API_ENTRYPOINT + (APP_ENV.API_ENTRYPOINT.endsWith("/") ? "" : "/");
 
   if (!resourceEndpoint || !entryPoint) return;
 
-  if (resourceEndpoint == '/people/companies/my')
+  if (resourceEndpoint == "/people/companies/my")
     console.log(entryPoint, resourceEndpoint, options.body);
 
   console.log(entryPoint, resourceEndpoint);
 
   return fetch(new URL(resourceEndpoint, entryPoint), options).then(
-    response => {
-      if (options.method == 'DELETE') return;
-      if (options.responseType == 'text') return response.text();
+    (response) => {
+      if (options.method == "DELETE") return;
+      if (options.responseType == "text") return response.text();
 
-      return response.json().then(json => {
-        if (json['@type'] == 'hydra:Error')
+      return response.json().then((json) => {
+        if (json["@type"] == "hydra:Error")
           throw {
-            message: json['hydra:description'],
+            message: json["hydra:description"],
+            code: response.status,
+            status: response.status,
+          };
+        if (json["@type"] == "ConstraintViolationList")
+          throw {
+            message: json["violations"],
             code: response.status,
             status: response.status,
           };
         return json;
       });
-    },
+    }
   );
 }
