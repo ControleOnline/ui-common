6618f86a182f90e58e91317bd60edd1b3841570a
Luiz Kim <luiz.kim@controleonline.com>
2026-02-10 14:25:31 -0300

    Change Routes

--- src/react/router/routes.js
+++ src/react/router/routes.js
@@ -1,18 +1,9 @@
-import React from 'react';
 import SettingsPage from '@controleonline/ui-common/src/react/pages/SettingsPage';
-import DefaultLayout from '@controleonline/ui-layout/src/react/layouts/DefaultLayout';
-
-
-const WrappedSettingsPage = ({navigation, route}) => (
-  <DefaultLayout navigation={navigation} route={route}>
-    <SettingsPage navigation={navigation} route={route} />
-  </DefaultLayout>
-);
 
 const commonRoutes = [
   {
     name: 'SettingsPage',
-    component: WrappedSettingsPage,
+    component: SettingsPage,
     options: {
       headerShown: true,
       title: 'Configurações',
